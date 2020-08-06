e5f8561211501b0d7367d33e358b8337a9904cb4
satoshigp <satoshi3104>
2020-08-06 09:27:17 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0806
+{
+    public partial class Form1 : Form
+    {
+        int vx = -10;
+        int vy = -10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          label1.Left +=vx;
+          label1.Top += vy;
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
